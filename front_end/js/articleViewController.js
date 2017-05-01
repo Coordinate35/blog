a37a213324e65db50de7a3c074134b10c24cbe0a
Coordinate35 <Coordinate35@hust.edu.cn>
2017-05-01 11:59:21 +0800

    render article

--- front_end/js/articleViewController.js
+++ front_end/js/articleViewController.js
@@ -30,7 +30,35 @@ function articleViewController() {
     }
 
     this._render = function(data) {
+        var articleInfo = {
+            "articleId": data.article_id,
+            "authorName": data.authorName,
+            "title": data.title,
+            "content": data.content,
+            "publishTime": data.publish_time,
+            "tags": data.tags
+        };
 
+        this._renderArticle(articleInfo);
+        this._renderRemark(data.comments);
+    }
+
+    this._renderArticle = function(articleInfo) {
+        var articleContainer = document.getElementById("article");
+        var articleNode = new articleDom();
+        var articleDom = articleNode.construct(data);
+        articleDom.setDescriptionNone();
+        articleDom.setContentContainerBlock();
+        articleContainer.appendChild(articleDom);
+    }
+
+    this._renderRemark = function(remarkList) {
+        var remarkContainer = document.getElementById("comment");
+        for (var i = 0; i < remarkList.length; ++i) {
+            var remarkNode = new remark();
+            var remarkDom = remarkNode.construct(remarkList[i]);
+            remarkContainer.appendChild(remarkDom);
+        }
     }
 }
 

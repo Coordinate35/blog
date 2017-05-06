7bc9e7ac07ffe0ce34bd14917335054cd672b2ad
Coordinate35 <Coordinate35@hust.edu.cn>
2017-05-06 14:34:06 +0800

    fix article image url path

--- api/application/config/article_image_upload_const.php
+++ api/application/config/article_image_upload_const.php
@@ -10,4 +10,4 @@ const ARTICLE_IMAGE_MAX_HEIGHT = 0;
 const UPLOAD_IMAGE_KEY = 'editormd-image-file';
 const ARTICLE_IMAGE_UPLOAD_SUCCESS = 1;
 const ARTICLE_IMAGE_UPLOAD_FAILED = 0;
-const ARTICLE_IMAGE_URL_PATH = 'upload/article';
+const ARTICLE_IMAGE_URL_PATH = 'upload/article_image';

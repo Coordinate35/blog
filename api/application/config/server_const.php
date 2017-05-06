7d102f688007f52dbe00bdbd1b57124a27f1d37e
Coordinate35 <Coordinate35@hust.edu.cn>
2017-05-06 14:38:12 +0800

    blog domain

--- api/application/config/server_const.php
+++ api/application/config/server_const.php
@@ -11,11 +11,14 @@ const COOKIE_DOMAIN = '.coordinate35.cn';
 switch (ENVIRONMENT) {
     case 'development':
         define("DOMAIN_NAME", "local.api.coordinate35.cn");
+        define("BLOG_DOMIAN", "local.blog.coordinate35.cn");
         break;
     case 'testing':
-        define("DOMAIN_NAME", "test.api.coordinate35.cn");
+        define("DOMAIN_NAME", "debug.api.coordinate35.cn");
+        define("BLOG_DOMIAN", "debug.blog.coordinate35.cn");
         break;
     case 'production':
         define("DOMAIN_NAME", "api.coordinate35.cn");
+        define("BLOG_DOMIAN", "blog.coordinate35.cn");
         break;
 }

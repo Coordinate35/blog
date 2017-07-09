bcfadfec553cdd8e2054d5e0bf47a35e6fddfdc5
Coordinate35 <Coordinate35@hust.edu.cn>
2017-07-09 21:09:33 +0800

    docker

--- service.sh
+++ service.sh
@@ -1,10 +1,10 @@
 if [ $1 == start ] ; then
     docker-compose -f docker-compose.yml up -d;
 elif [ $1 == build-start ] ; then
-    docker build -t coordinate35/production-blog-php:latest docker/php;
-    docker build -t coordinate35/production-blog-redis:latest docker/redis;
-    docker build -t coordinate35/production-blog-postgres:latest docker/postgres;
-    docker build -t coordinate35/production-blog-phppgadmin:latest docker/phppgadmin;
+    docker build -t coordinate35/production-blog-php:latest environment/php;
+    docker build -t coordinate35/production-blog-redis:latest environment/redis;
+    docker build -t coordinate35/production-blog-postgres:latest environment/postgres;
+    docker build -t coordinate35/production-blog-phppgadmin:latest environment/phppgadmin;
     docker-compose -f docker-compose.yml up -d;
 elif [ $1 == stop ] ; then
     docker stop blog_production-blog-php_1;
